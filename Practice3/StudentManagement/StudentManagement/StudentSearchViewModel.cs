05afafb3c51c0b324ef8af136feeb4a831991952
VoTriThong2001 <90382841+VoTriThong2001@users.noreply.github.com>
2021-10-09 16:34:02 +0700

    Exercise 1

---
+++ Practice3/StudentManagement/StudentManagement/StudentSearchViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Windows.Input;
+
+namespace StudentManagement
+{
+   
+
+    public class StudentSearchViewModel: BaseViewModel
+    {
+        private string m_searchKeyword;
+        public string SearchKeyword
+        {
+            get => m_searchKeyword;
+            set
+            {
+                m_searchKeyword = value;
+                OnPropertyChanged(nameof(SearchKeyword));
+            }
+        }
+
+        private string m_selectedClass;
+
+        public string SelectedClass
+        {
+            get => m_selectedClass;
+            set
+            {
+                m_selectedClass = value;
+                OnPropertyChanged(nameof(SelectedClass));
+            }
+        }
+
+        private Student m_selectedStudent;
+        public Student SelectedStudent
+        {
+            get => m_selectedStudent;
+            set
+            {
+                m_selectedStudent = value;
+                OnPropertyChanged(nameof(SelectedStudent));
+            }
+        }
+
+        public ICommand SearchCommand { get; set; }
+        public ICommand ResetCommand { get; set; }
+        public ICommand OpenDetailCommand { get; set; }
+
+        public ObservableCollection<Student> Students { get; set; }
+        public StudentSearchViewModel()
+        {
+            var jsonString = File.ReadAllText("Student_Data.json");
+            var students = JsonSerializer.Deserialize<List<Student>>(jsonString);
+            Students = new ObservableCollection<Student>(students);
+        }
+    }
+   
+
+}
---
+++ Practice3/StudentManagement/StudentManagement/StudenttDetailViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentManagement
+{
+    public class StudenttDetailViewModel
+    {   int StudentId;
+        string Firstname, Lastname, Gender, Email;
+        decimal Gpa;
+
+        public StudenttDetailViewModel(Student student)
+        {
+            
+            StudentId = student.StudentID;
+            Firstname = student.Firstname;
+            Lastname = student.Lastname;
+            Gender = student.Gender;
+            Email = student.Email;
+            Gpa = student.Gpa;
+
+        }
+    }
+}
