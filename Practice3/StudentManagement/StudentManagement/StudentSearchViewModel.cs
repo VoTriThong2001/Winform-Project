b83f3f20ca1991624e1cf24b36078742d3075d7d
VoTriThong2001 <90382841+VoTriThong2001@users.noreply.github.com>
2021-10-16 17:17:50 +0700

    Pratice 3 - Exercise 2

--- Practice3/StudentManagement/StudentManagement/StudentSearchViewModel.cs
+++ Practice3/StudentManagement/StudentManagement/StudentSearchViewModel.cs
@@ -8,7 +8,21 @@ using System.Windows.Input;
 
 namespace StudentManagement
 {
-   
+
+    public class Student
+    {
+
+        public int studentId { get; set; }
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public string email { get; set; }
+        public string gender { get; set; }
+        public string Class { get; set; }
+        public decimal gpa { get; set; }
+
+    }
+
+
 
     public class StudentSearchViewModel: BaseViewModel
     {
@@ -50,13 +64,25 @@ namespace StudentManagement
         public ICommand ResetCommand { get; set; }
         public ICommand OpenDetailCommand { get; set; }
 
+        public void DoOpenDetail()
+        {
+            var studentDetailViewModel = new StudenttDetailViewModel(SelectedStudent);
+            Window1 studentDetail = new Window1();
+            studentDetail.DataContext = studentDetailViewModel;
+            studentDetail.ShowDialog();
+        }
+
+
         public ObservableCollection<Student> Students { get; set; }
         public StudentSearchViewModel()
         {
             var jsonString = File.ReadAllText("Student_Data.json");
             var students = JsonSerializer.Deserialize<List<Student>>(jsonString);
             Students = new ObservableCollection<Student>(students);
+            OpenDetailCommand = new ConditionalCommand(x => DoOpenDetail());
         }
+
+
     }
    
 
