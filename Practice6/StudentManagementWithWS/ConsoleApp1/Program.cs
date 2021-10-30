aff99b6a8c7ab5ba907b113238a8d0eaed27bad8
VoTriThong2001 <90382841+VoTriThong2001@users.noreply.github.com>
2021-10-30 13:59:40 +0700

    responseTask changed

--- Practice6/StudentManagementWithWS/ConsoleApp1/Program.cs
+++ Practice6/StudentManagementWithWS/ConsoleApp1/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Net.Http;
 
-namespace TestStudentWS
+namespace StudentManagementWithWS
 {
     class Program
     {
@@ -10,7 +10,7 @@ namespace TestStudentWS
             using (var client = new HttpClient())
             {
                 client.BaseAddress = new Uri("https://localhost:44322/api/");
-                var responseTask = client.GetAsync("Students?keyword=test");
+                var responseTask = client.GetAsync("Students?hutechClass=19DTHQA2");
                 responseTask.Wait();
 
                 var result = responseTask.Result;
